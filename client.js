3bca763627c43902bb198d7d33d1bf2dc46127c8
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-11 15:49:52 -0500

    break in live solve

--- client.js
+++ client.js
@@ -49,9 +49,61 @@ console.log('array of employee data: ',  employees );
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
+  console.log(employee);
   // your logic here
-  
+  let bonusPercentage =0;
+
+  //- IF rating is <= 2 , no bonus
+  if (employee.reviewRating <= 2) {
+    bonusPercentage = 0;
+  }
+  //- IF rating is == 3, bonus = 4
+  if (employee.reviewRating == 3) {
+    bonusPercentage = 4;
+  }
+  //- IF rating is == 4, bonus = 6 
+  if (employee.reviewRating == 4) {
+    bonusPercentage = 6;
+  }
+  //- IF rating is == 5, bonus = 10
+  if (employee.reviewRating == 5) {
+    bonusPercentage == 10;
+  }
+  //- IF employee.number.length = 4, bonus +5
+  if(employee.employeeNumber.length == 4) {
+    bonusPercentage += 5;
+  }
+  //- IF income > $65,000, bonus -1
+  if(employee.annualSalary > 65000) {
+    bonusPercentage -= 1;
+  }
+  //- IF bonus > 13, bonus = 13
+  if (bonusPercentage > 13){
+    bonusPercentage = 13;
+  }
+  //- IF bonus < 0, bonus = 0
+  if (bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
+// The name property should contain the employee's name.
+//The bonusPercentage property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+//The totalCompensation property should be the adjusted annual compensation (base annual + bonus)
+//The totalBonus should be the employee's total bonus rounded to the nearest dollar.
   
   // return new object with bonus results
+  let result = {
+    something: "yay",
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: 'total compensation - TBD',
+    totalBonus: 'total bonus - TBD',
 
+  }
+  return result
+}
+
+// Testing for all employees
+for(let individual of employees) {
+  console.log(`%c${individual.name}`, 'color: black; background: yellow;');
+  calculateIndividualEmployeeBonus(individual);
 }
